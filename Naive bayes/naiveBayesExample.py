02a821679445df2a28185f8f6022fac826d0b281
José Machado <jmachado-out@redoute.pt>
2022-02-17 20:00:30 +0000

    Adds missing code to naive bayes

--- Naive bayes/naiveBayesExample.py
+++ Naive bayes/naiveBayesExample.py
@@ -1,5 +1,15 @@
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Instantiate the CountVectorizer method
+count_vector = CountVectorizer()
+
+# Fit the training data and then return the matrix
+training_data = count_vector.fit_transform(X_train)
+
+# Transform testing data and return the matrix. Note we are not fitting the testing data into the CountVectorizer()
+testing_data = count_vector.transform(X_test)
 
 naive_bayes = MultinomialNB()
 naive_bayes.fit(training_data, y_train)
